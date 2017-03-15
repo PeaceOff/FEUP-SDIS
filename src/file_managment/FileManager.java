2d859b1d3a453d6c8a4ba0a0f1dcb4409597f13c
David Joel Nogueira Azevedo <davidazevedo1996@hotmail.com>
2017-03-15 18:23:18 +0000

    Implemented hasmap to store fileid chunk_num and peers

--- src/file_managment/FileManager.java
+++ src/file_managment/FileManager.java
@@ -1,6 +1,9 @@
 package file_managment;
 
 import backup_service.protocols.ChannelManager;
+import com.sun.xml.internal.fastinfoset.algorithm.IntEncodingAlgorithm;
+import com.sun.xml.internal.fastinfoset.algorithm.IntegerEncodingAlgorithm;
+import utils.Debug;
 
 import java.io.*;
 import java.nio.file.FileAlreadyExistsException;
@@ -10,6 +13,9 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class FileManager {
 
@@ -17,6 +23,11 @@ public class FileManager {
     private String main_path;
     private MessageDigest hasher;
     private ChannelManager channels;
+    private HashMap<String,HashMap<Integer,HashSet<Integer>>> mapeador = new HashMap<String,HashMap<Integer,HashSet<Integer>>>();
+
+    public static void main(String[] args){
+        Debug.log("BOAS");
+    }
 
     public FileManager(ChannelManager c) throws IOException, NoSuchAlgorithmException {
         this.channels = c;
@@ -44,6 +55,7 @@ public class FileManager {
 
         for (int i = 0; i < len - this.chunk_size_bytes + 1; i += this.chunk_size_bytes) {
             //TODO enviar para o canal apropriado a mensagem de putchunk
+
             reader.read(chunks[n_chunks++], i, i + this.chunk_size_bytes);
         }
 
@@ -60,19 +72,97 @@ public class FileManager {
         return hasher.digest();
     }
 
-    public boolean save_chunck(byte[] chunkData, byte[] fileID, int chunk_num) throws IOException {
+    public boolean save_chunck(byte[] chunkData, byte[] fileID, int chunk_num, int senderID) throws IOException {
+
+        this.save_file_chunk_data(fileID,chunk_num,senderID);
 
         String directory = this.main_path + File.separator + fileID.toString();
         Path folder_path = Paths.get(directory);
-        if(!Files.exists(folder_path))//Ja existe a pasta
+        if(!Files.exists(folder_path))//Ainda nao existe a pasta
             Files.createDirectory(Paths.get(directory));
 
         Path chunk_path = Paths.get(directory + File.separator + Integer.toString(chunk_num));
-        if(Files.exists(chunk_path))
+        if(Files.exists(chunk_path)) {//Ja se guardou a chunk
+            //TODO enviar STORED
             return false;
+        }
 
         Files.write(chunk_path, chunkData);
 
         return true;
+    }
+
+    private void save_file_chunk_data(byte[] fileID, int chunk_num, int senderID){
+
+        String path_to_data = this.main_path + File.separator + fileID.toString() + File.separator + "data";
+
+        if(mapeador.containsKey(fileID.toString())){//Ja existe um mapeamento
+            HashMap<Integer,HashSet<Integer>> temp = mapeador.get(fileID.toString());
+            mapeador.put(fileID.toString(),helper_func(temp,chunk_num,senderID));
+        } else {//Verificar se o ficheiro existe
+            if(Files.exists(Paths.get(path_to_data))){//ler o ficheiro e serializar
+                HashMap<Integer,HashSet<Integer>> hmap = read_from_data_file(Paths.get(path_to_data));
+                mapeador.put(fileID.toString(),helper_func(hmap,chunk_num,senderID));
+            } else {
+                HashSet<Integer> hset = new HashSet<Integer>();
+                hset.add(senderID);
+                HashMap<Integer,HashSet<Integer>> hmap = new HashMap<Integer,HashSet<Integer>>();
+                hmap.put(chunk_num,hset);
+                mapeador.put(fileID.toString(),hmap);
+            }
+
+        }
+    }
+
+    private HashMap<Integer,HashSet<Integer>> helper_func(HashMap<Integer,HashSet<Integer>> hmap, int chunk_num, int senderID){
+
+        if(hmap.containsKey(chunk_num)){
+            hmap.get(chunk_num).add(senderID);
+        } else {
+            HashSet<Integer> hset = new HashSet<Integer>();
+            hset.add(senderID);
+            hmap.put(chunk_num,hset);
+        }
+        return hmap;
+    }
+
+    private HashMap<Integer,HashSet<Integer>> read_from_data_file(Path path) {
+
+        HashMap<Integer, HashSet<Integer>> res = null;
+        try
+        {
+            FileInputStream fis = new FileInputStream(path.toFile());
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            res = (HashMap) ois.readObject();
+            ois.close();
+            fis.close();
+        }catch(IOException e)
+        {
+            Debug.log("ERROR"," Failed to open file");
+        } catch (ClassNotFoundException e) {
+            Debug.log("ERROR"," Class not found at reading HashMap");
+        }
+        return res;
+    }
+
+    private void write_to_data_file(){
+
+        for (HashMap.Entry<String,HashMap<Integer,HashSet<Integer>>> entry : mapeador.entrySet()) {
+            String file_id = entry.getKey();
+            HashMap<Integer,HashSet<Integer>> hmap = entry.getValue();
+            String path = this.main_path + File.separator + file_id + File.separator + "data";
+
+            try {
+                FileOutputStream fos = new FileOutputStream(Paths.get(path).toFile());
+                fos.write(("").getBytes());
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                oos.writeObject(hmap);
+                oos.close();
+                fos.close();
+            }catch(IOException e)
+            {
+                Debug.log("ERROR"," Could not write file data");
+            }
+        }
     }
 }
