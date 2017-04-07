0a87302b2876ec8d88052dd97a13bfe603ed63c1
Antonio Passos Dias Aguiar Mota <davidazevedo1996@hotmail.com>
2017-04-07 14:41:41 +0100

    Added teste files and compile script

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -Xlint -d out -sourcepath src -encoding ISO-8859-1 src/backup_service/Client.java src/backup_service/Server.java
--- out/startServers.sh
+++ out/startServers.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 gnome-terminal -x bash -c "rmiregistry"
-echo -n "Number of Servers?"
-read nS
-echo -n "Starting at?"
-read sS
-total = $((nS + sS - 1))
-for i in {$sS..$total}
+set total=$(($1+$2-1))
+echo $total
+for (( i=$sS; i<=$total; i++ ))
 do
+	echo $i 
    gnome-terminal -x bash -c "java backup_service/Server 1.0 $i $i 224.1.1.1:1111 224.2.2.2:2222 224.3.3.3:3333"
 done
